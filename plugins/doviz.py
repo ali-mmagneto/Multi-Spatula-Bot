dd0d4e39a516eb8d4a7ccc18bc3c32ca8dfdb85b
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-12 11:19:33 +0300

    Update doviz.py

--- plugins/doviz.py
+++ plugins/doviz.py
@@ -1,6 +1,7 @@
 # :d
 from pyrogram import Client, filters
 from KekikSpatula import Doviz
+import json
 
 dovizler = Doviz()
 
@@ -9,9 +10,9 @@ async def dovizgetir(bot, message):
     sayi = 4 # Benim (:d) Gerekli Bulduğum ilk 4 Doviz Bilgisini Getirir. 
     text = "Dövizler:\n"
     say = 0
-    for key in json.loads(dovizler.gorsel())["veri"]:
+    for i in json.loads(dovizler.gorsel())["veri"]:
         say += 1
-        text += f"Birim: {key['birim']}\nAlış: {key['Gişe Alış']} TL\nSatış: {key['Gişe Satış']} Tl\n\n"
+        text += f"Birim: {i['birim']}\nAlış: {i['Gişe Alış']} TL\nSatış: {i['Gişe Satış']} Tl\n\n"
     if say == sayi:
         await message.reply_text(text)
         return
