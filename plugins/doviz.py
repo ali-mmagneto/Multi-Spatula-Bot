287f9e465d8658d7d1ca102d2f92e6625c682505
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-12 11:48:53 +0300

    Update doviz.py

--- plugins/doviz.py
+++ plugins/doviz.py
@@ -7,12 +7,15 @@ dovizler = Doviz()
 
 @Client.on_message(filters.command("doviz"))
 async def dovizgetir(bot, message):
-    sayi = 4 # Benim (:d) Gerekli Bulduğum ilk 4 Doviz Bilgisini Getirir. 
-    text = "Dövizler:\n"
-    say = 0
-    for i in json.loads(dovizler.gorsel())["veri"]:
-        say += 1
-        text += f"Birim: {i['birim']}\nAlış: {i['Gişe Alış']} TL\nSatış: {i['Gişe Satış']} Tl\n\n"
-    if say == int(sayi):
-        await bot.send_message(message.chat.id, text)
-        return
+    try:
+        sayi = 4 # Benim (:d) Gerekli Bulduğum ilk 4 Doviz Bilgisini Getirir. 
+        text = "Dövizler:\n"
+        say = 0
+        for i in json.loads(dovizler.gorsel())["veri"]:
+            say += 1
+            text += f"Birim: {i['birim']}\nAlış: {i['Gişe Alış']} TL\nSatış: {i['Gişe Satış']} Tl\n\n"
+        if say == int(sayi):
+            await bot.send_message(message.chat.id, text)
+            return
+    except Exception as e:
+        await bot.send_message(message.chat.id, "`{e}`"
