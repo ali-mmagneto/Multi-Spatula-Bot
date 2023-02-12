bae416bdbd170bef14d9d66d2e7473022ff666f8
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-12 11:36:34 +0300

    Create ezan.py

---
+++ plugins/ezan.py
@@ -0,0 +1,26 @@
+# :d
+from pyrogram import Client, filters 
+from KekikSpatula import Ezan
+import json
+
+
+@Bot.on_message(filters.command('ezan'))
+async def ezangetir(bot, message):
+    try:
+        ev = unidecode(message.text).split()
+        if len(ev) < 2:
+            bot.send_message(message.chat.id, "Hatalı Kullanım :/ Doğru Kullanım Şu Şekilde:\n\n/ezan İstanbul") 
+            return
+        il = ev[1]
+        istek = Ezan(il)
+        text = ""
+        for i in json.loads(istek.gorsel())["veri"]:
+            text += f"{i['il']} İçin Ezan Saatleri:\nİmsak: `{i['imsak']}`\nGüneş: `{i['gunes']}`\nÖğle: `{i['ogle']}`\nİkindi: `{i['ikindi']}`\nAkşam: `{i['aksam']}`\nYatsı: `{i['yatsi']}`"
+        await bot.send_message(
+           chat_id=message.chat.id,
+           text=text)
+    except Exception as e:
+        await bot.send_message(
+            chat_id=message.chat.id,
+            text=f"`{e}`")
+        
