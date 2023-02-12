c5c3590a240b683a8d701b78925af4b41a058385
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-12 12:20:26 +0300

    Create bim.py

---
+++ plugins/bim.py
@@ -0,0 +1,17 @@
+from pyrogram import Client, filters
+from KekikSpatula import BimAktuel
+import json
+
+@Client.on_message(filters.command("bim"))
+async def bimgetir(bot, message):
+    try:
+        aktuel = BimAktuel()
+        text = ""
+        for i in json.loads(aktuel.gorsel())["veri"]:
+            text += f"{i['urun_baslik']\n\n**Fiyat**: {i['urun_fiyat']\n**Detay**: {i['urun_link']}"
+            await bot.send_photo(
+                chat_id=message.chat.id, 
+                photo=photo,
+                caption=text)
+    except Exception as e:
+        await bot.send_message(message.chat.id, f"`{e}`")
