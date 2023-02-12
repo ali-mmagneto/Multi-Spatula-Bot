4b1b08be76bff560d57f74d985ee51df2fd595f8
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-12 12:02:50 +0300

    Update doviz.py

--- plugins/doviz.py
+++ plugins/doviz.py
@@ -12,7 +12,7 @@ async def dovizgetir(bot, message):
     say = 0
     text = "**Dövizler:**\n\n"
     for i in json.loads(dovizler.gorsel())["veri"]:
-        say += 1
+        say += 1 # döngüyü kaç kere tekrar ettiğini saydırıyoruz.
         text += f"**Birim**: `{i['birim']}`\n**Alış**: `{i['Gişe Alış']} TL`\n**Satış**: `{i['Gişe Satış']} TL`\n\n"
         if int(say) == int(sayi):
             await message.reply_text(text) 
