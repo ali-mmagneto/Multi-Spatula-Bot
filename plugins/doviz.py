fddda09ce693c88f85ea191c89c8f67b867832c9
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-12 12:00:59 +0300

    Update doviz.py

--- plugins/doviz.py
+++ plugins/doviz.py
@@ -8,7 +8,11 @@ import json
 @Client.on_message(filters.command("doviz"))
 async def dovizzz(bot, message):
     dovizler = Doviz()
+    sayi = 4
+    say = 0
     text = "**Dövizler:**\n\n"
     for i in json.loads(dovizler.gorsel())["veri"]:
-        text += f"**Birim**: `{i['birim']}`\n**Alış**: `{i['Gişe Alış']} TL`\nSatış: `{i['Gişe Satış']} TL`\n"
-    await message.reply_text(text) 
+        say += 1
+        text += f"**Birim**: `{i['birim']}`\n**Alış**: `{i['Gişe Alış']} TL`\n**Satış**: `{i['Gişe Satış']} TL`\n\n"
+        if int(say) == int(sayi):
+            await message.reply_text(text) 
