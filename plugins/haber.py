617d6ab149b6e15800ace14efd6f791ca6ebf6a7
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-12 11:28:49 +0300

    Create haber.py

---
+++ plugins/haber.py
@@ -0,0 +1,22 @@
+# :d
+from pyrogram import Client, filters
+from KekikSpatula import SonDakika
+import json
+
+
+@Bot.on_message(filters.command("haber"))
+async def habergetir(bot, message):
+    haber = SonDakika()
+    sayi = 1
+    say = 0
+    text = ""
+    for i in json.loads(haber.gorsel())["veri"]:
+        say += 1
+        text += f"Başlık: {i['haber']}\n\nDetay: {i['link']}"
+        photo = f"{i['gorsel']}"
+        if say == sayi:
+            await bot.send_photo(
+                chat_id = message.chat.id, 
+                photo = photo,
+                caption = text)
+            return
