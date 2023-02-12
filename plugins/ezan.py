dcce796771dced4c8275d21f1f7a05ef87e4cec0
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-12 12:04:36 +0300

    Update ezan.py

--- plugins/ezan.py
+++ plugins/ezan.py
@@ -9,13 +9,13 @@ async def ezangetir(bot, message):
     try:
         ev = unidecode(message.text).split()
         if len(ev) < 2:
-            bot.send_message(message.chat.id, "Hatalı Kullanım :/ Doğru Kullanım Şu Şekilde:\n\n/ezan İstanbul") 
+            await bot.send_message(message.chat.id, "Hatalı Kullanım :/ Doğru Kullanım Şu Şekilde:\n\n/ezan İstanbul") 
             return
         il = ev[1]
         istek = Ezan(il)
         text = ""
         for i in json.loads(istek.gorsel())["veri"]:
-            text += f"{i['il']} İçin Ezan Saatleri:\nİmsak: `{i['imsak']}`\nGüneş: `{i['gunes']}`\nÖğle: `{i['ogle']}`\nİkindi: `{i['ikindi']}`\nAkşam: `{i['aksam']}`\nYatsı: `{i['yatsi']}`"
+            text += f"{i['il']} İçin **Ezan Saatleri**:\n\n**İmsak**: `{i['imsak']}`\n**Güneş**: `{i['gunes']}`\n**Öğle**: `{i['ogle']}`\n**İkindi**: `{i['ikindi']}`\n**Akşam**: `{i['aksam']}`\n**Yatsı**: `{i['yatsi']}`"
         await bot.send_message(
            chat_id=message.chat.id,
            text=text)
