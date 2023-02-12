6dbf8d94de7a8a1cef7f6122de8370912d81c6be
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-12 11:18:19 +0300

    Create hava.py

---
+++ plugins/hava.py
@@ -0,0 +1,25 @@
+from pyrogram import Client, filters
+from KekikSpatula import HavaDurumu
+from unidecode import unidecode
+import json 
+
+@Bot.on_message(filters.command('hava'))
+async def havaa(bot, message):
+    try:
+        ev = unidecode(message.text).split()
+        if len(ev) < 3:
+            bot.send_message(message.chat.id, "Hatalı Kullanım :/ Doğru Kullanım Şu Şekilde:\n\n/hava İstanbul Avcılar") 
+            return
+        il = ev[1]
+        ilce = ev[2]
+        istek = HavaDurumu(il, ilce)
+        text = ""
+        for i in json.loads(istek.gorsel())["veri"]:
+            text += f"{i['yer']} İçin:\nHava Durumu: `{i['derece']}`\nVakit: `{i['gun']}`"
+        await bot.send_message(
+           chat_id=message.chat.id,
+           text=text)
+    except Exception as e:
+        await bot.send_message(
+            chat_id=message.chat.id,
+            text=f"`{e}`")
