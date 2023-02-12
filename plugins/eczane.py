50f7e11e0ced93fa465fbd3be94546b1561b053d
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-12 13:07:51 +0300

    Create eczane.py

---
+++ plugins/eczane.py
@@ -0,0 +1,22 @@
+from pyrogram import Client, filters
+from KekikSpatula import NobetciEczane
+import json
+from unidecode import unidecode
+
+@Client.on_message(filters.command('nobetcieczane'))
+async def eczanegetir(bot, message):
+    try:
+        link = unidecode(message.text).split()
+        if len(link) < 3:
+            await message.reply_text("Hatalı Kullanım :/ Doğru Kullanım\n\n/nobetcieczane İstanbul Kadıköy")
+            return
+        else:
+            il =  link[1]
+            ilce = link[2]
+        text = ""
+        nobetci = NobetciEczane(il, ilce)
+        for i in json.loads(nobetci.gorsel())["veri"]:
+            text += f"{i['ad']} {i['adres']}"
+        await message.reply_text(text)
+    except Exception as e:
+        await message.reply_text("`{e}`")
