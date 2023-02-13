b54aa9bec35a13235709f7eb9b6b6b26449e41bb
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-13 13:01:03 +0300

    Create imdb.py

---
+++ plugins/imdb.py
@@ -0,0 +1,20 @@
+from PyMovieDb import IMDB
+from pyrogram import Client, filters
+import json
+
+@Client.on_message(filters.command('imdb'))
+async def imdbgetir(bot, message):
+    try:
+        mes = message.text.split(" ", 1)
+        if len(mes) == 1:
+            await message.reply_text("Hatalı Kullanım :/ Doğru kullanım:\n\n`/imdb Wandavision`")
+        else:
+            aranacak = mes[1]
+        imdbbilgi = IMDB()
+        istek = imdbbilgi.get_by_name(<name>, tv=False)
+        text = "" 
+        for i in json.loads(istek):
+            text += f"{i['name']}\n\n{i['description']}"
+        await message.reply_text(text)
+    except Exception as e:
+        await message.reply_text(f"`{e}`")
