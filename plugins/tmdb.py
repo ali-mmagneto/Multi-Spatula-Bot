6ab4f524f90a622c946120dc87d89ecad0a138bd
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-13 15:52:15 +0300

    Update tmdb.py

--- plugins/tmdb.py
+++ plugins/tmdb.py
@@ -1,4 +1,4 @@
-from PyMovieDb import IMDB
+# Coded by :d
 from pyrogram import Client, filters
 import requests
 import json
@@ -7,7 +7,7 @@ from PIL import Image
 @Client.on_message(filters.command('tmdb'))
 async def tmdbgetir(bot, message):
     try:
-        sayi = 2
+        sayi = 2 # benim uygun gördüğüm gösterilecek film sayısı
         mes = message.text.split(" ", 1)
         if len(mes) == 1:
             await message.reply_text("Hatalı Kullanım :/ Doğru kullanım:\n\n`/tmdb Bullet Train`")
